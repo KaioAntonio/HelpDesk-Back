013221b6d3020c08b5c7b80d63d8ecd1e14816ee
Kaio Antônio <kaioaar@gmail.com>
2023-02-15 15:32:08 -0300

    add: anotacoes JPA para classes de dominio

--- src/main/java/com/kaio/helpdesk/domain/Cliente.java
+++ src/main/java/com/kaio/helpdesk/domain/Cliente.java
@@ -1,2 +1,33 @@
-package com.kaio.helpdesk.domain;public class Cliente {
+package com.kaio.helpdesk.domain;
+
+import com.kaio.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Cliente extends Pessoa{
+    private static  final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Cliente(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
+        super(id, nome, cpf, email, senha);
+        addPerfis(Perfil.CLIENTE);
+    }
+
+    public Cliente() {
+        addPerfis(Perfil.CLIENTE);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
 }
--- src/main/java/com/kaio/helpdesk/domain/Tecnico.java
+++ src/main/java/com/kaio/helpdesk/domain/Tecnico.java
@@ -1,6 +1,35 @@
-package com.kaio.helpdesk.domain.enums;
+package com.kaio.helpdesk.domain;
 
-import com.kaio.helpdesk.domain.Pessoa;
 
+import com.kaio.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Tecnico extends Pessoa {
+    private static  final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "tecnico")
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico() {
+        super();
+        addPerfis(Perfil.TECNICO);
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+        super(id, nome, cpf, email, senha);
+        addPerfis(Perfil.TECNICO);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
 }
