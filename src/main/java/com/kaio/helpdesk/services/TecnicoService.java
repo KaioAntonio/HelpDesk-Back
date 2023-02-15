ce7841feaa339a108debed24a590bf066634f87f
Kaio Antônio <kaioaar@gmail.com>
2023-02-15 19:52:02 -0300

    add: endpoint findById do tecnico

--- src/main/java/com/kaio/helpdesk/services/TecnicoService.java
+++ src/main/java/com/kaio/helpdesk/services/TecnicoService.java
@@ -1,2 +1,22 @@
-package com.kaio.helpdesk.services;public class TecnicoService {
+package com.kaio.helpdesk.services;
+
+import com.kaio.helpdesk.domain.Tecnico;
+import com.kaio.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+
+
 }
