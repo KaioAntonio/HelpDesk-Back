5fe032cb8147ffade7ad51934b5e837953b26320
Kaio Antônio <kaioaar@gmail.com>
2023-02-19 17:13:52 -0300

    add: Exceção para objeto não encontrado

--- src/main/java/com/kaio/helpdesk/services/TecnicoService.java
+++ src/main/java/com/kaio/helpdesk/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.kaio.helpdesk.services;
 
 import com.kaio.helpdesk.domain.Tecnico;
 import com.kaio.helpdesk.repositories.TecnicoRepository;
+import com.kaio.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id));
     }
 
 
