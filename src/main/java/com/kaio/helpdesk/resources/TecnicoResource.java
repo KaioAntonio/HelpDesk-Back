29a225736e3ac5f8bcd21855f77a190d4104525c
Kaio Antônio <kaioaar@gmail.com>
2023-03-21 23:50:10 -0300

    add: autorizacao com JWT

--- src/main/java/com/kaio/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/kaio/helpdesk/resources/TecnicoResource.java
@@ -6,6 +6,7 @@ import com.kaio.helpdesk.services.TecnicoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -35,6 +36,7 @@ public class TecnicoResource {
 
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
         Tecnico newObg = tecnicoService.create(objDTO);
@@ -42,7 +44,7 @@ public class TecnicoResource {
         return ResponseEntity.created(uri).build();
 
     }
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id,
                                              @Valid @RequestBody TecnicoDTO objDTO) {
@@ -50,7 +52,7 @@ public class TecnicoResource {
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
 
     }
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
         tecnicoService.delete(id);
